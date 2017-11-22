94802641a69e3e4971d6c96354c258a1648b5b00
Matt Rosen <matt@mrosen.com>
2017-11-22 16:32:06 -0500

    Added aksdwi and removed bellyrub

--- scripts/bottracker.js
+++ scripts/bottracker.js
@@ -15,7 +15,8 @@ $(function () {
       { name: 'upgoater', interval: 2.4, comments: true, min_bid: 0.05 },
       { name: 'voter', interval: 2.4, comments: true, min_bid: 0.05 },
       { name: 'appreciator', interval: 2.4, comments: false, min_bid: 0.05 },
-      { name: 'pushup', interval: 2.4, comments: true, min_bid: 0.05 }
+      { name: 'pushup', interval: 2.4, comments: true, min_bid: 0.05 },
+      { name: 'aksdwi', interval: 2.4, comments: false, min_bid: 0.1, max_bid: 5 }
       /*{ name: 'khoa', interval: 2.4 },
       { name: 'polsza', interval: 2.4 },
       { name: 'drotto', interval: 2.4 }*/
