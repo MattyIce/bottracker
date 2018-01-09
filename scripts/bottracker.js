e1a049b764b6c3665b76547f795756e5a905642f
Matt Rosen <matt@mrosen.com>
2018-01-09 05:19:04 -0500

    Fixed price feed

--- scripts/bottracker.js
+++ scripts/bottracker.js
@@ -73,11 +73,15 @@ $(function () {
 
     function loadPrices() {
       // Load the current prices of STEEM and SBD
-      $.get('https://postpromoter.com/api/prices/', function (data) {
-        sbd_price = parseFloat(data.sbd_price);
-        steem_price = parseFloat(data.steem_price);
-        $('#sbd_price').text(sbd_price.formatMoney());
+      $.get('https://api.coinmarketcap.com/v1/ticker/steem/', function (data) {
+        steem_price = parseFloat(data[0].price_usd);
         $('#steem_price').text(steem_price.formatMoney());
+      });
+
+      // Load the current prices of STEEM and SBD
+      $.get('https://api.coinmarketcap.com/v1/ticker/steem-dollars/', function (data) {
+        sbd_price = parseFloat(data[0].price_usd);
+        $('#sbd_price').text(sbd_price.formatMoney());
       });
     }
     loadPrices();
