9878062c9d8a05f50ed7cd9a55afd34787ed496f
Matt Rosen <matt@mrosen.com>
2018-01-29 21:10:04 -0500

    Sorted bot list in vote calculator

--- scripts/bottracker.js
+++ scripts/bottracker.js
@@ -10,6 +10,12 @@ $(function () {
     $.get('https://steembottracker.com/bidbots.json', function (data) {
       bots = data;
 
+      bots.sort(function(a, b){
+        if(a.name < b.name) return -1;
+        if(a.name > b.name) return 1;
+        return 0;
+      });
+
       bots.forEach(function (bot) {
         bot_names.push(bot.name);
         $('#bot_list').append('<option value="' + bot.name + '">' + bot.name + '</option>');
